080afd4fe0bda6aaec709acd61a40e250c8692bd
kurouw <github0220@gmail.com>
2016-05-04 05:23:06 +0900

    output log

--- reqCafe/reqCafe.go
+++ reqCafe/reqCafe.go
@@ -16,7 +16,7 @@ type Dataset struct {
 func RtCafeInfo(calltime time.Time)string{
 	// Loading jsonfile
 	fg := 0
-	file, err := ioutil.ReadFile("./config.json")
+	file, err := ioutil.ReadFile("./reqCafe/config.json")
 	var datasets []Dataset
 	json_err := json.Unmarshal(file, &datasets)
 	if err != nil{
