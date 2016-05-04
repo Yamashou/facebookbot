66e5a818987fd82778c659980ac5ed649fb87fe6
Shota Yamamota <1230124fw@gmail.com>
2016-05-04 11:14:42 +0900

    add infomation pusher

---
+++ info.go
@@ -0,0 +1,121 @@
+package main
+
+import(
+  "fmt"
+
+)
+
+func infometion()string{
+  return "http://web-int.u-aizu.ac.jp/labs/istc/ipc/index.html"
+}
+func literacy1()string{
+  return "http://web-int.u-aizu.ac.jp/course/lit1/"
+}
+func literacy2()string{
+  return "http://web-int.u-aizu.ac.jp/course/lit2/"
+}
+func sisugaiOUT()string{
+  return "http://www.u-aizu.ac.jp/course/csI/"
+}
+func sisugaiIN()string{
+  return "file:///home/course/csI/public_html/2016/welcome.html"
+}
+func rekougaku()string{
+  return "http://web-int.u-aizu.ac.jp/course/cselab/"
+}
+func InformationSecurity()string{
+  return "http://web-int.u-aizu.ac.jp/~yodai/course/IS/welcome.html"
+}
+func Multimedia3()string{
+  return "http://web-int.u-aizu.ac.jp/~asada/class/MMS.html"
+}
+func Multimedia2()string{
+  return "http://www.u-aizu.ac.jp/~shigeo/course/mms/"
+}
+func ComputerNet2()string{
+  return "http://web-int.u-aizu.ac.jp/~pham/cn1/"
+}
+func ComputerNet1()string{
+  return " http://web-int.u-aizu.ac.jp/~pham/cn1/"
+}
+func prog0OUT()string{
+  return "http://www.u-aizu.ac.jp/course/prog0/"
+}
+func prog0IN()string{
+  return "file:///home/course/prog0/public_html/2016/welcome.html"
+}
+func prog1OUT()string{
+  return "http://www.u-aizu.ac.jp/course/prog1/"
+}
+func prog1IN()string{
+  return "file:///home/course/prog1/public_html/2016/welcome.html"
+}
+func java1()string{
+  return "http://web-int.u-aizu.ac.jp/~vkluev/courses/javaone/"
+}
+func CPP()string{
+  return "授業中に指示されます"
+}
+func ComputerGengo()string{
+  return "http://provit.u-aizu.ac.jp/clang/"
+}
+func algo()string{
+  return "AOJでググれ"
+}
+func algotoku()string{
+  return "http://hare.u-aizu.ac.jp/classaa/2013"
+}
+func gengoshori()string{
+  return "http://elvis.rowan.edu/~bergmann/books.html."
+}
+func jyouhouassyuku()string{
+  return "http://web-int.u-aizu.ac.jp/~asada/class/DC.html"
+}
+func keisannkikagaku()string{
+  return "http://i-health.u-aizu.ac.jp/CompuGeo/index.html"
+}
+func Softwarekougaku()string{
+  return "http://borealis.u-aizu.ac.jp/classes/se1"
+}
+func AI()string{
+  return "http://web-ext.u-aizu.ac.jp/~qf-zhao/TEACHING/AI/AI.html"
+}
+func ComputerGraphics()string{
+  return "http://web-int.u-aizu.ac.jp/~fayolle/teaching/2015/cg/index.html"
+}
+func gashori()string{
+  return "http://iplab.u-aizu.ac.jp/moodle/"
+}
+func gashoriHand()string{
+  return "http://iplab.u-aizu.ac.jp/moodle/"
+}
+func baio()string{
+  return "http://web-ext.u-aizu.ac.jp/course/bmclass/"
+}
+func robot()string{
+  return "http://iplab.u-aizu.ac.jp/moodle"
+}
+func hyuumann()string{
+  return "http://web-int.u-aizu.ac.jp/~mcohen/welcome/courses/AizuDai/undergraduate/HI&VR"
+}
+func dezitaru()string{
+  return " http://www.dspguide.com/pdfbook.htm"
+}
+func Webengin()string{
+  return "http://web-int.u-aizu.ac.jp/~vkluev/courses/webengineering/"
+}
+func Softwarekougaku2()string{
+  return "http://sealpv0.u-aizu.ac.jp/moodle/"
+}
+func SofteareDtdio()string{
+  return "http://borealis.u-aizu.ac.jp/classes/studio/"
+}
+func bunsann()string{
+  return " www.tinyurl.com/aizu-dc"
+}
+func main(){
+  fmt.Println(infometion())
+  fmt.Println(literacy1())
+
+
+}
