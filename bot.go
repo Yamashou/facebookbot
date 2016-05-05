d0463341f6d6ab055e7d274181137cebc74f6e58
kurouw <github0220@gmail.com>
2016-05-05 15:36:25 +0900

    clean

--- bot.go
+++ bot.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/facebookbot/reqCafe"
-	"github.com/facebookbot/fbmessenger"
-	"github.com/facebookbot/infoSub"
+	"github.com/kurouw/reqCafe"
+	"github.com/acomagu/fbmessenger-go"
+	"github.com/kurouw/infoSub"
 	"regexp"
 	"time"
 //	"bytes"
