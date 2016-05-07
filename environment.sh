a94200c9093cfdfcb6a1f842a854a252f39fe785
Yuki Ito <acomagu@gmail.com>
2016-05-07 11:59:47 +0900

    organize

---
+++ environment.sh
@@ -0,0 +1,5 @@
+export LINE_CHANNEL_ID=1466468325
+export LINE_CHANNEL_SECRET=cdf9c4a2f5a3ffbf9a6f49b23a2e4714
+export LINE_MID=ud7becb7baf9c55f6bb495d4f332ed181
+export PORT=443
+export ENDPOINT_NAME=line
