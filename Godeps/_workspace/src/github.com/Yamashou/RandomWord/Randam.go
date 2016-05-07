8a8f272ba06e0784e6640ce823c0b966589d0245
Yuki Ito <acomagu@gmail.com>
2016-05-07 18:31:40 +0900

    update RandomWord

--- Godeps/_workspace/src/github.com/Yamashou/RandomWord/Randam.go
+++ Godeps/_workspace/src/github.com/Yamashou/RandomWord/Randam.go
@@ -1,58 +1,47 @@
-package main
+package randomword
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
-	"os"
 	"time"
 )
 
+// Word express each word
 type Word struct {
 	Word string `json:"word"`
 }
 
-func randam_word(InWord string){
-  var T [1000]Word
-  fmt.Println("set")
+// RandomWord return word the user sent in past
+func RandomWord(InWord string) string {
+	var T [1000]Word
+	fmt.Println("set")
 	rand.Seed(time.Now().UnixNano())
 	file, err := ioutil.ReadFile("./RWord.json")
 	var datasets []Word
-	json_err := json.Unmarshal(file, &datasets)
+	jsonerr := json.Unmarshal(file, &datasets)
 	if err != nil {
-		fmt.Println("Format Error: ", json_err)
+		fmt.Println("Format Error: ", jsonerr)
 	}
 	i := 0
-  k := 0
-  fg := 0
+	k := 0
+	fg := 0
 	for i = range datasets {
 		if datasets[i].Word == InWord {
-      fg +=1
+			fg++
 			break
 		}
 
 	}
-  for j := range datasets{
-    T[j] = datasets[j]
+	for j := range datasets {
+		T[j] = datasets[j]
 
-    if datasets[j].Word ==""{
-      break
-    }
-    k++
-  }
-
-	if fg != 1 {
-		T[k].Word = InWord
-		bytes, _ := json.Marshal(T)
-		ioutil.WriteFile("./RWord.json", bytes, os.ModePerm)
-	} else {
-    ky := rand.Intn(10)
-    fmt.Println(ky)
-	fmt.Println(datasets[ky].Word)
+		if datasets[j].Word == "" {
+			break
+		}
+		k++
 	}
-}
 
-func main() {
-	randam_word("米うま〜〜〜〜〜〜")
+	return datasets[k].Word
 }
