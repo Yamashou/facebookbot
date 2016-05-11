3ed533162e67e4b2f363f536366b4836f5e68d92
Yuki Ito <acomagu@gmail.com>
2016-05-11 10:01:16 +0900

    Created testmsg module. This is for debug or confirm the result of generated reply message.

---
+++ testmsg/testmsg.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+
+	"github.com/m2mtu/facebookbot/reply"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(">>> ")
+	for scanner.Scan() {
+		fmt.Println(reply.Get(scanner.Text()))
+		fmt.Print(">>> ")
+	}
+}
